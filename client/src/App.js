0ca579fb6cdffa6fadd9f6c5b452447efbd0850a
Shrutik <shrutik.mali@somaiya.edu>
2021-09-23 11:26:39 +0530

    Some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Prescriber from './Prescriber/Prescriber';
 
 const App = () => {
-  return <h3>App</h3>
+  return (
+    <Router>
+      <Switch>
+        <Route path='/prescriber'>
+          <Prescriber />
+        </Route>
+        <Route path='/'>
+          <h3>App</h3>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/Prescriber/Prescriber.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+
+const Prescriber = () => {
+  return <Button variant='contained'>Button</Button>
+}
+
+export default Prescriber;
